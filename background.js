65ce7df48b0b682cde52e8e5d3c4d01154c0a792
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-08-23 19:25:40 +0200

    updated background.js

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
-console.log("Background script running");
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url && tab.url.includes("amazon.com")) {
+    const queryParameters = tab.url.split("?")[1];
+    console.log("query parameters: " + queryParameters);
+    const urlParameters = new URLSearchParams(queryParameters);
+    console.log("query parameters: " + urlParameters.get("pd_rd_r"));
 
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension installed");
-});
-
-// Example: Listen for messages from content or popup scripts
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Message received", request);
-    sendResponse({ message: "Background script received the message" });
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      productId: urlParameters.get("pd_rd_r"),
+    });
+  }
 });
--- contentScript.js
+++ contentScript.js
@@ -5,13 +5,3 @@ const titleElement = document.querySelector('h1');
 if (titleElement) {
     console.log("Product title:", titleElement.innerText);
 }
-
-let div = document.createElement(div);
-div.id = 'toolbar';
-document.body.appendChild(div);
-
-fetch(chrome.runtime.getURL('popup.html'))
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById('toolbar').innerHTML = data;
-  });
