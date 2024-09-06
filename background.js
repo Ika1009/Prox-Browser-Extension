d4cd3be98915ddf2c56104df4612a2a5f9d3a58d
Ilija Doncic <doncic.ilija1009@gmail.com>
2024-09-06 17:27:54 +0200

    fetchGoogleShoppingProductData

--- background.js
+++ background.js
@@ -22,7 +22,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
+
 const fetchProductData = async (productName) => {
+
+};
+
+
+const fetchAmazonProductData = async (productName) => {
   console.log(productName);
   try {
       const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
@@ -56,3 +62,40 @@ const fetchProductData = async (productName) => {
       console.error('Error in fetchProductData:', error);
   }
 };
+
+
+const fetchGoogleShoppingProductData = async (productName) => {
+  console.log(productName);
+  try {
+      const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Basic ' + btoa('_prox_RQ99U:R8kTz3_bG4sQ') // Replace with your own credentials
+          },
+          body: JSON.stringify({
+              source: 'google_shopping_search',  // Changed to Google Shopping
+              domain: 'com',  // Use the appropriate domain, such as 'com' for US
+              query: productName,
+              parse: true,
+              context: [
+                  {
+                      key: 'autoselect_variant',
+                      value: true
+                  }
+              ]
+          })
+      });
+
+      if (response.ok) {
+          const data = await response.json();
+          return data.results;
+      } else {
+          const errorDetails = await response.text();
+          console.error('Failed to fetch product data:', errorDetails);
+      }
+  } catch (error) {
+      console.error('Error in fetchGoogleShoppingProductData:', error);
+  }
+};
+
